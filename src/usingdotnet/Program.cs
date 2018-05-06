de3f6778136ac55dc4533db463f0c92d7a8eae4a
Timothy Lim <tim.lim@intercom.io>
2018-05-06 22:46:43 +0800

    Update

---
+++ src/usingdotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Intercom.Clients;
+using Intercom.Core;
+
+namespace usingdotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsersClient usersClient = new UsersClient(new Authentication("MyPersonalAccessToken"));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
